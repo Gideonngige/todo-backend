b6a70cc8c7ef0fa0ceb0dffe1778354b155e9611
Gideonngige <ushindigideon01@gmail.com>
2025-11-08 14:19:04 +0300

    removed api keys

--- src/config/firebaseClient.ts
+++ src/config/firebaseClient.ts
@@ -1,13 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import dotenv from "dotenv";
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9okcuaDZQIEfp3TrAIFyK75_lOhiO8gM",
-  authDomain: "baustaka-89295.firebaseapp.com",
-  projectId: "baustaka-89295",
-  storageBucket: "baustaka-89295.firebasestorage.app",
-  messagingSenderId: "708089267933",
-  appId: "1:708089267933:web:457d61a9a1662a31d67d34"
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID
 };
 
 const app = initializeApp(firebaseConfig);
